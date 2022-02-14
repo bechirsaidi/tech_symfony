47239c2c47ef0575cab3c6a1d44e6d8dc92a5e4b
bechirsaidi <bachirbobsaidi@gmail.com>
2022-02-14 22:30:33 +0100

    routeur + partie de twig + filtre + fonctions

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+
+    #[Route('/template', name: 'template')]
+    public function template() {
+        return $this->render('template.html.twig');
+    }
+    #[Route('/order/{maVar}', name: 'test.order.route')]
+    public function testOrderRuote($maVar) {
+        return new Response("
+        <html><body>$maVar</body></html>
+        ");
+    }
+
+    #[Route('/first', name: 'first')]
+    public function index(): Response
+    {
+        // chercher au la abse de données vos users
+        return $this->render('first/index.html.twig', [
+            'name' => 'Saidi',
+            'firstname' => 'Bechir'
+        ]);
+    }
+
+//    #[Route('/sayHello/{name}/{firstname}', name: 'say.hello')]
+    public function sayHello(Request $request, $name, $firstname): Response
+    {
+        return $this->render('first/hello.html.twig', [
+            'nom' => $name,
+            'prenom' => $firstname
+        ]);
+    }
+
+    #[Route(
+        'multi/{entier1<\d+>}/{entier2<\d+>}',
+        name: 'multiplication'
+    )]
+    public function multiplication($entier1, $entier2) {
+        $resultat = $entier1 * $entier2;
+        return new Response("<h1>$resultat</h1>");
+    }
+}
